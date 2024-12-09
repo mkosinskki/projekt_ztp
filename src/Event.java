357dcd1bcaa53fa0d5bc4cffe95150c59db55991
lkuci <lkucikowicz@gmail.com>
2024-12-09 15:23:13 +0100

    GameHistory with Builder pattern

---
+++ src/Event.java
@@ -0,0 +1,28 @@
+class Event {
+    private String player; // Kto wykonał ruch (np. "Player 1" lub "AI")
+    private String action; // Typ akcji (np. "shoot", "placeShip")
+    private String details; // Szczegóły akcji (np. "at position (2,3)", "missed", "hit")
+
+    public Event(String player, String action, String details) {
+        this.player = player;
+        this.action = action;
+        this.details = details;
+    }
+
+    public String getPlayer() {
+        return player;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Player: %s, Action: %s, Details: %s", player, action, details);
+    }
+}
